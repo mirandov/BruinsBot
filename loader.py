526e3d0ca47fa7c022f3a2a0f8688aeaf26a4d46
ldmirandov <ldmirandov@gmail.com>
2024-11-23 04:20:58 +0300

    delete files

--- loader.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.fsm.storage.memory import MemoryStorage
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-import config
-bot = Bot(token=config.BOT_TOKEN)
-dp = Dispatcher(storage=MemoryStorage())
-scheduller = AsyncIOScheduler()
