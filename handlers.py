1832710432d81ff73f1ccee117005fd86f81db90
ldmirandov <ldmirandov@gmail.com>
2024-11-24 23:23:34 +0300

    Full version mvp1

--- handlers.py
+++ handlers.py
@@ -13,12 +13,64 @@ bot = Bot(token=config.BOT_TOKEN)
 async def send_message_to_test(msg: Message):
     markup = build_keyboard()
     await msg.answer(f'{config.POST_INIT}', parse_mode='html', reply_markup=markup)
+
 async def get_admins(chat_id):
     chat_admins = (await bot.get_chat_administrators(chat_id)).copy()
     result = []
     for admin in chat_admins:
         result.append(admin.user.id)
     return result
+
+async def add_coach_to_ul(callback):
+    old_message = callback.message.text
+    prefix_message = old_message.split('Тренера:')[0]
+    message_split_coach = old_message.split('Тренера:')[1].split('\n')
+    message_split_coach = [i for i in message_split_coach if i]
+    add_accepted_list = f'{callback.from_user.first_name} {callback.from_user.last_name} (@{callback.from_user.username}) \n'
+    check_in_list = False
+    if (old_message.find(callback.from_user.username) != -1):
+        check_in_list = True
+    if (check_in_list):
+        return ''
+    else:
+        message_split_coach.append(add_accepted_list)
+        message_split_coach_str = (','.join(message_split_coach)).replace(',', '\n')
+        return (prefix_message + "Тренера:\n\n" + message_split_coach_str)
+    
+async def add_player_accept_to_ul(callback):
+    old_message = callback.message.text
+    prefix_message = old_message.split('Будут на тренировке: \n')[0]
+    ul_accepted_player = old_message.split('Будут на тренировке: \n')[1].split('❌ Будут отсутствовать на тренировке:')[0].split('\n')
+    ul_accepted_player = [i for i in ul_accepted_player if i]
+    postfix_message = old_message.split('Будут на тренировке: \n')[1].split('❌ Будут отсутствовать на тренировке:')[1]
+    add_accepted_list = f'{callback.from_user.first_name} {callback.from_user.last_name} (@{callback.from_user.username}) \n'
+    check_in_list = False
+    if (old_message.find(callback.from_user.username) != -1):
+        check_in_list = True
+    if (check_in_list):
+        return ''
+    else:
+        ul_accepted_player.append(add_accepted_list)
+        ul_accepted_player_str = (','.join(ul_accepted_player)).replace(',', '\n')
+        return (prefix_message + "Будут на тренировке: \n" + ul_accepted_player_str + "\n❌ Будут отсутствовать на тренировке:" + postfix_message)
+    
+async def add_player_dicline_to_ul(callback):
+    old_message = callback.message.text
+    prefix_message = old_message.split('Будут отсутствовать на тренировке: \n')[0]
+    ul_diclined_player = old_message.split('Будут отсутствовать на тренировке: \n')[1].split('👤 Тренера:')[0].split('\n')
+    ul_diclined_player = [i for i in ul_diclined_player if i]
+    postfix_message = old_message.split('Будут отсутствовать на тренировке: \n')[1].split('👤 Тренера:')[1]
+    add_accepted_list = f'{callback.from_user.first_name} {callback.from_user.last_name} (@{callback.from_user.username}) \n'
+    check_in_list = False
+    if (old_message.find(callback.from_user.username) != -1):
+        check_in_list = True
+    if (check_in_list):
+        return ''
+    else:
+        ul_diclined_player.append(add_accepted_list)
+        ul_diclined_player_str = (','.join(ul_diclined_player)).replace(',', '\n')
+        return (prefix_message + "Будут отсутствовать на тренировке: \n" + ul_diclined_player_str + "\n👤 Тренера:" + postfix_message)
+
 
 
 @router.message(Command("start"))
@@ -40,28 +92,30 @@ async def job_handler(msg: Message):
 
 @router.callback_query(F.data == accept_traning_callback_data)
 async def handle_accept_traning_(callback_query: CallbackQuery):
-    await callback_query.answer(text="Вы записаны на тренировку", show_alert=True)
+    check_already_answer = await add_player_accept_to_ul(callback_query)
+    if (check_already_answer == ''):
+        await callback_query.answer(text="Уже голосовали", show_alert=True)
+    else:
+        await callback_query.answer(text="Красавчик! Каждая тренировка делает тебя сильнее💪", show_alert=True)
+        await bot.edit_message_text(reply_markup= callback_query.message.reply_markup, chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id, text=check_already_answer, inline_message_id=callback_query.inline_message_id)
 
 @router.callback_query(F.data == decline_traning_callback_data)
 async def handle_decline_traning_(callback_query: CallbackQuery):
-    await callback_query.answer(text="Причину об отсутсвие оставьте в комментарии", show_alert=True)
+    check_already_answer = await add_player_dicline_to_ul(callback_query)
+    if (check_already_answer == ''):
+        await callback_query.answer(text="Уже голосовали", show_alert=True)
+    else:
+        await callback_query.answer(text="Причину об отсутсвие напиши ответом на сообщние ⬇️", show_alert=True)
+        await bot.send_message(callback_query.message.chat.id, f'@{callback_query.from_user.username} напиши причину отсутсвия на тренировке!', parse_mode='html')
+        await bot.edit_message_text(reply_markup= callback_query.message.reply_markup, chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id, text=check_already_answer, inline_message_id=callback_query.inline_message_id)
+
 
 @router.callback_query(F.data == accept_coach_traning_callback_data)
 async def handle_accept_coach_traning(callback_query: CallbackQuery):
-    old_message = callback_query.message.text
-    prefix_message = old_message.split('Тренера:')[0]
-    message_split_coach = old_message.split('Тренера:')[1].split('\n')
-    add_accepted_list = f'{callback_query.from_user.first_name} {callback_query.from_user.last_name} (@{callback_query.from_user.username}) \n'
-    check_in_list = False
-    for i in message_split_coach:
-        if (i!=''):
-            username = i.split('@')[1].split(')')[0]
-            if(username == callback_query.from_user.username):
-                check_in_list = True
-    if (check_in_list):
+    check_already_answer = await add_coach_to_ul(callback_query)
+    if (check_already_answer == ''):
         await callback_query.answer(text="Уже голосовали", show_alert=True)
     else:
-        message_split_coach.append(add_accepted_list)
-        new_message = prefix_message + "Тренера:" + '\n'.join(message_split_coach)
-        await bot.edit_message_text(reply_markup= callback_query.message.reply_markup, chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id, text=new_message, inline_message_id=callback_query.inline_message_id)
+        await callback_query.answer(text="Моё почтение 🦸‍♂️", show_alert=True)
+        await bot.edit_message_text(reply_markup= callback_query.message.reply_markup, chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id, text=check_already_answer, inline_message_id=callback_query.inline_message_id)
   
--- inline_keyboards.py
+++ inline_keyboards.py
@@ -5,7 +5,7 @@ decline_traning_callback_data = "decline_traning_callback_data"
 accept_coach_traning_callback_data = "accept_coach_traning_callback_data"
 def build_keyboard() -> InlineKeyboardMarkup:
     accept_traning_btn1 = InlineKeyboardButton(text= "✅ Буду на тренировке", callback_data=accept_traning_callback_data)
-    decline_traning_btn1 = InlineKeyboardButton(text= "❌ Не будет на тренировке", callback_data=decline_traning_callback_data)
+    decline_traning_btn1 = InlineKeyboardButton(text= "❌ Не буду на тренировке", callback_data=decline_traning_callback_data)
     accept_coach_traning_btn1 = InlineKeyboardButton(text= "🏈 Тренер", callback_data=accept_coach_traning_callback_data)
     row_first = [accept_traning_btn1, decline_traning_btn1]
     row_second = [accept_coach_traning_btn1]
